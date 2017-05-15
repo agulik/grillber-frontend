6b00a3e9ccef7b1d54c8bac6f6b591b223099f6e
Andrew Gulik <andrewgulik@gmail.com>
2017-05-15 14:44:00 -0400

    created initial route structure, added relevant component files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+// routes explained
+
+// App = main App
+// Home = main dashboard page
+// LandingPage = redirect if not logged in
+// LearnMore = panel state load for more info
+// Login = panel state load if 'already have an account'
+// Orders = previous orders / create new order page
+// NewOrder = step 1 of new order page
+
+
+const routes = (
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+            <Route path="auth" component={LandingPage}>
+              <Route path="learn-more" component={LearnMore} />
+              <Route path="login" component={Login} />
+            </Route>
+            <Route path="orders" component={Orders}>
+              <Route path="new-order" component={NewOrder} />
+            </Route>
+        </Route>
+    </Router>
 );
+
+ReactDOM.render(routes, document.getElementById('root'));

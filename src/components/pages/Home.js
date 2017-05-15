3ca32d2b24e8afd7f3efa2ae604b22314e1f7110
Andrew Gulik <andrewgulik@gmail.com>
2017-05-15 16:06:12 -0400

    successfully added materialize css files to project

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Button, Icon} from 'react-materialize';
 import './Home.css';
 
 
@@ -14,6 +15,7 @@ export default class Home extends Component {
     return (
       <div className="home">
         <h1> the home page !! :) </h1>
+        <Button node='a' waves='light'><Icon right>file_cloud</Icon>button</Button>
       </div>
     );
   }

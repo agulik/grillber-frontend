124fd061839909cb2c57d9cac8ed85dd3836a922
Andrew Gulik <andrewgulik@gmail.com>
2017-05-16 10:03:07 -0400

    fixed spacing for navbar

--- src/components/elements/GrillberNav.js
+++ src/components/elements/GrillberNav.js
@@ -5,7 +5,7 @@ import './GrillberNav.css';
 export default (props) => (
   <div className='nav'>
     <Navbar brand='Grillber' right className="z-depth-0">
-      <NavItem href='get-started.html'><Icon>menu</Icon></NavItem>
+      <NavItem href=''><Icon>menu</Icon></NavItem>
     </Navbar>
   </div>
 );

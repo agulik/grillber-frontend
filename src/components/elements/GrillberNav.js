5a1d58fda6be01fc75a1bf28d51a72c783b67f55
Andrew Gulik <andrewgulik@gmail.com>
2017-05-17 19:28:49 -0400

    changed grillber nav to a class based component

--- src/components/elements/GrillberNav.js
+++ src/components/elements/GrillberNav.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Icon, Col, Row, Modal} from 'react-materialize';
+import auth from '../../auth';
 import './GrillberNav.css';
 
-export default(props) => (
-  <div className='nav'>
-    <Row>
-      <Col s={12}>
-        <a href="/"><h2>Grillber</h2></a>
-        <Modal
-          trigger={< a href='' onClick={() => props._handleMenuClick()}><Icon>menu</Icon></a>}>
-          <div className="close modal-action modal-close" onClick={() => props.closeMenu()}>
-            <Icon>close</Icon>
-          </div>
-          <a href=''><p>FAQ</p></a>
-          <a href=''><p>Settings</p></a>
-          <a href=''><p>Logout</p></a>
-        </Modal>
-      </Col>
-    </Row>
-  </div>
-);
+
+
+export default class GrillberNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  _handleLogout = () => {
+        if (localStorage.token) {
+          auth.logout()
+          .then(localStorage.removeItem('token'))
+          .catch(console.error);
+        }
+      else {
+        this.setState({ error: "You are not logged in!"});
+      }
+    }
+
+  render() {
+
+    return (
+      <div className='nav'>
+        <Row>
+          <Col s={12}>
+            <a href="/"><h2>Grillber</h2></a>
+            <Modal
+              trigger={< a href='' onClick={() => this.props._handleMenuClick()}><Icon>menu</Icon></a>}>
+              <div className="close modal-action modal-close" onClick={() => this.props.closeMenu()}>
+                <Icon>close</Icon>
+              </div>
+              <a href=''><p>FAQ</p></a>
+              <a href=''><p>Settings</p></a>
+              <a href='' onClick={this._handleLogout}><p>Logout</p></a>
+            </Modal>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}

03b111833718ead7a9f5c5db3371693ee91e7df6
Andrew Gulik <andrewgulik@gmail.com>
2017-05-17 14:15:42 -0400

    finished base functions for signup component, have to tweak the handle sign up function - the if statement in specific

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Icon, Col, Row, Input, Button} from 'react-materialize';
 import './Signup.css';
 
+const ENTER = 13;
+
 export default class Signup extends Component {
   constructor() {
     super();
@@ -31,9 +33,46 @@ export default class Signup extends Component {
     }
   }
 
+  _handleSignUp = () => {
+
+    let {error} = this.state;
+
+    let {
+      firstname: {value: firstname},
+      lastname: {value: lastname},
+      phone: {value: phone},
+      email: {value: email},
+      password: {value: password},
+      confirmpassword: {value: confirmpassword}
+    } = this.refs;
+
+    if (firstname && lastname && phone && email) {
+      if (password === confirmpassword) {
+        // auth.signup(email, password)
+        // .then(res => this.props.router.push('/login'))
+        // .catch(console.error);
+      } else {
+        this.setState ({
+          error: "your passwords do not match"
+        })
+      }
+    } else {
+      this.setState({ error: "Please complete all the fields"});
+    }
+  }
+
+  _handleEnter = (e) => {
+    if (this.state && this.state.error) {
+      this.setState({ error: null });
+    }
+    if (e.keyCode===ENTER) {
+      this._handleSignUp();
+    }
+  }
+
   render() {
 
-    let {currentInput} = this.state;
+    let {currentInput, error} = this.state;
 
     return (
       <div className="signup-background">
@@ -42,16 +81,17 @@ export default class Signup extends Component {
         </div>
         <div className="signup-form">
           <Row>
-            <Input s={6} label="First Name"/>
-            <Input s={6} label="Last Name"/>
-            <Input s={12} label="Phone" onInput={this._handlePhoneInput} value={currentInput}/>
-            <Input s={12} type="email" label="Email"/>
-            <Input s={12} type="password" label="password"/>
-            <Input s={12} type="password" label="confirm password"/>
+            <Input s={6} label="First Name" ref="firstname" />
+            <Input s={6} label="Last Name" ref="lastname" />
+            <Input s={12} label="Phone" onInput={this._handlePhoneInput} value={currentInput} ref="phone"/>
+            <Input s={12} type="email" label="Email" ref="email" />
+            <Input s={12} type="password" label="password" ref="password" />
+            <Input s={12} type="password" label="confirm password" ref="confirmpassword" onKeyUp={this._handleEnter}/>
           </Row>
         </div>
         <div className="next-section">
-          <Button>Sign up</Button>
+          <Button onClick={this._handleSignUp}>Sign up</Button>
+          <span className="error">{error}</span>
         </div>
       </div>
     );

42d787cda8b0502ccdb91bc29642c3a914e38d0f
Andrew Gulik <andrewgulik@gmail.com>
2017-05-17 11:15:10 -0400

    pretty much finished grillber hero reusable component

--- src/components/elements/GrillberHero.js
+++ src/components/elements/GrillberHero.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {Icon, Col, Row, Modal} from 'react-materialize';
+import {Icon, Col, Row, Modal, Button} from 'react-materialize';
 import './GrillberHero.css';
 
 export default(props) => (
   <div className="background">
     <h2>Grillber</h2>
+    <p className="tagline">BBQ delivery service at the touch of a button</p>
+    <div className="button-div">
+      <Button>learn more</Button>
+    </div>
   </div>
 );
